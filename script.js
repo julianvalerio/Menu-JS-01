20b2404b04b5cae18330c45ed7a66a82a72ec9f3
julianvalerio <julianrvalerio89@gmail.com>
2023-09-20 17:43:29 -0300

    Script

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function fixNav() {
     }
 }
 window.addEventListener('scroll',fixNav);
+
+var imgs =document.querySelectorAll('.slide img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0;
+const interval = 2000;
+var timer = setInterval(changeSlide, interval);
+function changeSlide(n){
+    for(var i=0; i < imgs.length; i++){
+        imgs[i].style.opacity = 0;
+        dots[i].className = dots[i].className.replace(' active','');
+    }
+    currentImg = (currentImg + 1) % imgs.length;
+    if(n != undefined){
+        clearInterval(timer);
+        timer = setInterval(changeSlide, interval);
+        currentImg = n;
+    }
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+}
